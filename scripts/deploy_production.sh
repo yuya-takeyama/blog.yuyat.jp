f35617a7b6a234869ae53b07c0c40e347528fb5f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2022-05-04 21:19:26 +0900

    Change branch for debugging purposes

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -15,15 +15,15 @@ GIT_REPO="git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.g
 git submodule init
 git submodule update
 
-remote=`git ls-remote --heads 2> /dev/null | grep gh-pages || true`
+remote=`git ls-remote --heads 2> /dev/null | grep gh-pages-debug || true`
 
 if [ -n "$remote" ]; then
-  git clone -b gh-pages "${GIT_REPO}" public
+  git clone -b gh-pages-debug "${GIT_REPO}" public
   rm -rf public/*
 else
   git init public
   cd public
-  git checkout -b gh-pages
+  git checkout -b gh-pages-debug
   git remote add origin "${GIT_REPO}"
   cd ..
 fi
@@ -35,4 +35,4 @@ git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 git add --all
 git commit -m 'Update [ci skip]'
-git push -f origin gh-pages
+git push -f origin gh-pages-debug
